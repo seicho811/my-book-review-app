88a26e6cf79b7d394eb84aad4c11cc5121cc1adc
seicho811 <hw.th61@gmail.com>
2025-08-21 19:35:10 +0900

    feat: Add simple validation for login form

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -1,10 +1,35 @@
-import { useState } from "react";
+import { useState, type FormEvent } from "react";
+
+type Errors = {
+  email: boolean;
+  password: boolean;
+};
 
 export default function LoginForm() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [errors, setErrors] = useState<Errors>({
+    email: false,
+    password: false,
+  });
 
-  function handleSubmit() {
+  function handleSubmit(e: FormEvent) {
+    e.preventDefault();
+
+    const newErrors = {
+      email: !email,
+      password: !password,
+    };
+
+    setErrors(newErrors);
+
+    if (newErrors.email || newErrors.password) {
+      return;
+    }
+
+    setErrors({ email: false, password: false });
+    console.log({ email: email, password: password });
+
     return;
   }
 
@@ -16,24 +41,28 @@ export default function LoginForm() {
           id="email"
           name="email"
           type="email"
+          autoComplete="email"
           value={email}
           onChange={(e) => {
             setEmail(e.target.value);
           }}
         />
       </div>
+      {errors.email && <div className="alert">Email is required</div>}
       <div>
-        <label htmlFor="passsowrd">Password</label>
+        <label htmlFor="password">Password</label>
         <input
           id="password"
           name="password"
           type="password"
+          autoComplete="current-password"
           value={password}
           onChange={(e) => {
             setPassword(e.target.value);
           }}
         />
       </div>
+      {errors.password && <div className="alert">Password is required</div>}
       <button type="submit">Login</button>
     </form>
   );
