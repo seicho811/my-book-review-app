98bb48d1d5a1b8c9aca6e1037b1bc0703d999bba
seicho811 <hw.th61@gmail.com>
2025-08-24 14:55:57 +0900

    fix

--- e2e/login.test.ts
+++ e2e/login.test.ts
@@ -1,21 +1,28 @@
 import { test, expect } from "@playwright/test";
 
-test("should login when fields with valid values", async ({ page }) => {
+test("should show error message when any fields is empty", async ({ page }) => {
+  await page.goto("/login");
+  await page.getByRole("button", { name: /login/i }).click();
+  expect(page.getByRole("alert")).toHaveCount(2);
+});
+
+test("should login with no error messages when fields with valid values", async ({
+  page,
+}) => {
   await page.goto("http://localhost:5173/login");
 
   await page.getByLabel(/email/i).fill("aaa@a.com");
   await page.getByLabel(/password/i).fill("11111");
+  await page.getByRole("button", { name: /login/i }).click();
 
-  await Promise.all([
-    page.getByRole("button", { name: /login/i }).click(),
-    page.waitForURL(/http:\/\/localhost:5173\/home$/),
-  ]);
-  expect(page).toHaveURL("http://localhost:5173/home");
+  // await expect(page.getByRole("alert")).toHaveCount(0);
+  await expect(page).toHaveURL("http://localhost:5173/home");
 });
 
 test("should not login when fileds are no-filled ", async ({ page }) => {
   await page.goto("/login");
   await page.getByRole("button", { name: /login/i }).click();
+  await expect(page.getByRole("alert")).toHaveCount(2);
   await expect(page).toHaveURL("/login");
 });
 
